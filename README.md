72b5d65f34b33b83cc01f8fa2c4c53d248dccfc9
Kaidyn Phelan <kaidyn.phelan@simple.io>
2022-10-12 21:52:05 +1100

    Created React app and set up express server

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # project-kimber
+
+# Project Description:
+Application to store and compare shooting scores for traing and tracking improvment.
+Plans to add the ability to score.
---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, {useEffect, useState} from 'react'
+
+function App () {
+  return (
+    <div>
+      <p>
+        Welocme to the scoring app xD
+      </p>
+    </div>
+  )
+}
+
+export default App;
